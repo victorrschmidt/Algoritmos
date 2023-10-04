be513de91360b7a1dc2ac114d1330f7b3680229b
Victor <vicoschmidt@gmail.com>
2023-10-04 08:12:02 -0300

    edit bfs

---
+++ src/grafos/bfs_menor_caminho/bfs_menor_caminho.cpp
@@ -0,0 +1,99 @@
+// --------------------------------------------------------------------------------
+// BREADTH FIRST SEARCH - MENOR CAMINHO EM UM GRAFO NÃO VALORADO
+// --------------------------------------------------------------------------------
+
+/*
+* Complexidade: O(V+E)
+*
+* A ideia do algoritmo é visitar todos os vértices adjacentes ao vértice de origem,
+* depois visitar todos os vértices adjacentes a esses, e assim sucessivamente.
+* Cada camada de busca corresponde a um movimento - a distância mínima possível
+* entre o vértice que está sendo visitado e o vértice de origem.
+* 
+* Assim, é possível determinar a menor distância entre o vértice de origem e 
+* todos os outros vértices do grafo (considerando que cada aresta tem peso 1). 
+* A distância do vértice de origem até cada um dos vértices será armazenada 
+* no array dist[]. Como inicialmente não sabemos a distância até cada vértice,
+* iremos definir todas as distância como 'infinito'. Conforme fazemos a travessia 
+* no grafo, armazenamos a distância até o vértice u em dist[u] (onde u é o número do vértice).
+*
+* Ao final, teremos o array definido com todas as distâncias e, as distâncias 
+* que estiverem ainda como 'infinito' indicam que aquele vértice não é conexo 
+* com o vértice de origem. Assim, podemos responder a menor distância entre 
+* o vértice de origem e qualquer outro vértice do grafo em O(1).
+*
+* O grafo utilizado de exemplo está no mesmo diretório deste arquivo.
+*/
+
+#include <bits/stdc++.h>
+#define INF int(1e9)
+using namespace std;
+
+const int vertices = 13;  // Número de vértices do grafo de exemplo
+
+vector<int> grafo[vertices] = {  // Lista de adjacências do grafo de exemplo
+    {6,7},  // 0 se liga com 6 e 7
+    {7,4,12},  // 1 se liga com 7, 4 e 12
+    {3,6,8},  // 2 se liga com 3, 6 e 8
+    {5,6,2},  // ...
+    {1,8},
+    {3},
+    {0,3,2},
+    {0,1},
+    {2,9,4},
+    {8,10},
+    {9,11},
+    {10,12},
+    {1,11}
+};
+int dist[vertices];  // Array para armazenar a distância do vértice de origem até os outros vértices
+
+// Função bfs
+void bfs(int origem){
+
+    for(int i = 0; i < vertices; i++){
+        dist[i] = INF;  // Definimos a distância até os outros vértices como 'infinito'
+    }
+
+    dist[origem] = 0;  // A distância da origem até a própria origem é 0
+
+    bool visitado[vertices] = {false};  // Array de vértices visitados (todos os elementos inicializados como 'false')
+    visitado[origem] = true;  // Definir o vértice de origem como 'visitado'
+
+    queue<int> fila;  // Fila para verificar as adjacências de cada vértice explorado
+    fila.push(origem);  // Adicionar o vértice de origem à fila
+    
+    int v_restantes = 1;  // Vértices restantes na camada de busca (inicialmente só com o vértice de origem). Será utilizado para determinar o número de vértices em cada camada de busca
+    int distancia = 1;  // Distância até os outros vértices, inicialmente 1 (1° camada de busca)
+
+    while(!fila.empty()){  // Iteração para cada vértice na fila
+        int v = fila.front(); fila.pop();  // Pegar o último vértice e removê-lo da fila
+
+        for(int u : grafo[v]){  // Iteração para cada vértice adjacente à 'v'
+            if(!visitado[u]){  // Se o vértice u não foi visitado
+                visitado[u] = true;  // Agora visitamos esse vértice
+                fila.push(u);  // Adicioná-lo à fila
+                dist[u] = distancia;  // Definir a distância até esse vértice
+            }
+        }
+
+        v_restantes--;  // Diminuir o número de vértices na camada de busca
+        if(v_restantes == 0){  // Se foram analisados todos os vértices nessa camada de busca
+            v_restantes = fila.size();  // A próxima camada de busca tem todos os vértices na próxima camada (tamanho atual da fila)
+            distancia++;  // Como teremos que analisar outra camada de busca, a distância aumenta em 1
+        }
+        
+    }
+
+}
+
+int main(){
+    bfs(0);  // dist[] = {0, 2, 2, 2, 3, 3, 1, 1, 3, 4, 5, 4, 3}
+
+    bfs(5);  // dist[] = {3, 5, 2, 1, 4, 0, 2, 4, 3, 4, 5, 6, 6}
+
+    bfs(8);  // dist[] = {3, 2, 1, 2, 1, 3, 2, 3, 0, 1, 2, 3, 3}
+
+
+    return 0;
+}
