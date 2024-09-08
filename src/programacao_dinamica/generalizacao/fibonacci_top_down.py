d8d1c13116b0c876382c317b4ca9f1b10583b3ce
Victor Schmidt <vicoschmidt@gmail.com>
2024-09-08 07:58:22 -0300

    Explicação para pd/generalizacao

--- src/programacao_dinamica/generalizacao/fibonacci_top_down.py
+++ src/programacao_dinamica/generalizacao/fibonacci_top_down.py
@@ -3,22 +3,22 @@
 # --------------------------------------------------------------------------------
 
 dp = [0 for i in range(100)]  # Lista de memorização
-ok = [False for i in range(100)]  # Lista para verificar se um estado arbitrário da dp já foi calculado
+calculado = [False for i in range(100)]  # Lista para verificar se um estado arbitrário da dp já foi calculado
 N = 10  # Limite
 
 # Função recursiva
 def fib(n: int) -> int:
-    if ok[n]:  # Se o estado n da dp já foi calculado
+    if calculado[n]:  # Se o estado n da dp já foi calculado
         return dp[n]  # Retornar o estado n da dp
 
-    ok[n] = True  # Definir o estado n como 'calculado'
+    calculado[n] = True  # Definir o estado n como 'calculado'
     dp[n] = fib(n-2) + fib(n-1)  # Definir o estado n da dp como a soma dos estados n-2 e n-1
     return dp[n]  # Retornar o estado n da dp
 
 dp[1] = 0  # Caso base
-ok[1] = True
+calculado[1] = True
 dp[2] = 1  # Caso base
-ok[2] = True
+calculado[2] = True
 
 fib(N)  # Chamar a função para o último estado da dp (limite)
 
