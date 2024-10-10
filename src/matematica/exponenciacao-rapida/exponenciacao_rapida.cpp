83578cb3ad6c8154c0fdcb936de7bcac6ad9aebb
Victor Schmidt <vicoschmidt@gmail.com>
2024-10-10 20:08:22 -0300

    Arquivos e diretórios revisados

---
+++ src/matematica/exponenciacao-rapida/exponenciacao_rapida.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+// Função recursiva para calcular b**e
+int potencia(int b, int e) {
+    // Caso base: b**0 é igual a 1
+    if (e == 0) {
+        return 1;
+    }
+
+    // Se o expoente for ímpar
+    if (e & 1) {
+        // Retornar b * b**(e-1)
+        return b * potencia(b, e - 1);
+    }
+
+    // Se o expoente for par, definir p = b**(e/2)
+    int p = potencia(b, e / 2);
+
+    // Retornar p*p = b**(e/2) * b**(e/2)
+    return p*p;
+}
+
+int main() {
+
+    return 0;
+}
